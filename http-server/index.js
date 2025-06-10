52446890b53f8a850225b683e6c040b11e5bc86c
selin <emlaselin@gmail.com>
2025-06-10 23:25:19 +0530

    Remove todo.js from todo-cli

--- http-server/index.js
+++ http-server/index.js
@@ -7,21 +7,26 @@ const args = minimist(process.argv.slice(2));
 const port = args.port || 3000;
 
 const server = http.createServer((req, res) => {
-  let file = 'home.html';
-  if (req.url === '/project') file = 'project.html';
-  else if (req.url === '/registration') file = 'registration.html';
-
-  fs.readFile(path.join(__dirname, file), 'utf8', (err, data) => {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('Page not found');
-    } else {
+  if (req.url === '/registration') {
+    const filePath = path.join(__dirname, 'registration.html');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end('Error loading registration page');
+        return;
+      }
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.end(data);
-    }
-  });
+    });
+    return;
+  }
+
+  // Other routes like home, project, etc.
+
+  res.writeHead(404, {'Content-Type': 'text/plain'});
+  res.end('Page Not Found');
 });
 
 server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
