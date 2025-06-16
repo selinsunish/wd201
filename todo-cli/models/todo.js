54e64fdb2f732ab383568d9b819bbe289a2a7cbd
selinsunish <emlaselin@gmail.com>
2025-06-16 20:39:57 +0530

    Update todo.js

--- todo-cli/models/todo.js
+++ todo-cli/models/todo.js
@@ -1,4 +1,3 @@
-// todo.js
 "use strict";
 const { Model } = require("sequelize");
 
@@ -9,23 +8,9 @@ module.exports = (sequelize, DataTypes) => {
     }
 
     displayableString() {
-      const UNCHECKED_BOX = "[ ]";
-      const CROSS_BOX = "[x]";
-      const today = new Date().toISOString().split("T")[0];
-
-      let checkbox;
-      if (this.dueDate < today && !this.completed) {
-        checkbox = CROSS_BOX;
-      } else if (!this.completed) {
-        checkbox = UNCHECKED_BOX;
-      } 
-      const needsDueDate = this.dueDate !== today || this.completed;
-      const datePart = needsDueDate ? this.dueDate : "";
-
-      return `${this.id}. ${checkbox} ${this.title} ${datePart}`.trim();
+      let checkbox = this.completed ? "[x]" : "[ ]";
+      return `${this.id}. ${checkbox} ${this.title} ${this.dueDate}`;
     }
-
-    // add other static methods if needed
   }
 
   // ✅ THIS LINE MUST BE OUTSIDE THE CLASS
@@ -36,10 +21,3 @@ module.exports = (sequelize, DataTypes) => {
       completed: DataTypes.BOOLEAN,
     },
     {
-      sequelize,
-      modelName: "Todo",
-    }
-  );
-
-  return Todo;
-};
